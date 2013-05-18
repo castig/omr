2a758e8406004f34b997392856435e98204b0289
castig <castig@gmail.com>
2013-05-17 20:00:04 -0400

    yo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omr::Application.routes.draw do
+
+  root :to => 'pages#index'
+  get "about" => "pages#about"
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
